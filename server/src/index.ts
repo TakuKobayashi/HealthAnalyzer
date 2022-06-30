22bd8d0e59fe23d2972a7593ce640d480a2a1f9c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-30 12:40:32 +0900

    cookieを使えるようにしておく

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
+import fastifyCookie from '@fastify/cookie';
 
 import { lineBotRouter } from './routes/line/bot';
 import { lineNotifyRouter } from './routes/line/notify';
@@ -7,6 +8,7 @@ import { withingsAuthRouter } from './routes/withings/auth';
 import { withingsWebhookRouter } from './routes/withings/webhook';
 
 const app = fastify();
+app.register(fastifyCookie);
 
 app.get('/', (request, reply) => {
   reply.send({ hello: 'world' });

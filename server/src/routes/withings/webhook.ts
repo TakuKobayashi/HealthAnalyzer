c1cc2b47a486fbd273dbc03cf4943db855f724ab
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-07-09 21:06:53 +0900

    使わなくなったものを消した

--- server/src/routes/withings/webhook.ts
+++ server/src/routes/withings/webhook.ts
@@ -1,8 +1,5 @@
-import axios, { AxiosResponse } from 'axios';
-import { stringify, parse } from 'query-string';
-import { createHmac } from 'crypto';
 import { setupFireStore } from '../../common/firestore';
-import { RequestTokenSignatureBasic, WithingsAccount } from '../../interfaces/withings';
+import { WithingsAccount } from '../../interfaces/withings';
 import { WithingsApi } from '../../common/withings';
 import { withingsUsersCollectionName } from '../../types/withings';
 
@@ -37,44 +34,4 @@ export async function withingsWebhookRouter(app, opts): Promise<void> {
     console.log(payload);
     res.send('OK');
   });
-}
-
-async function constructNonceSignature(action: string): Promise<RequestTokenSignatureBasic> {
-  const nonce = await requestNonse();
-  // See this: https://developer.withings.com/developer-guide/v3/get-access/sign-your-requests/
-  const signature = createHmac('sha256', process.env.WITHINGS_API_SECRET)
-    .update([action, process.env.WITHINGS_API_CLIENT_ID, nonce].join(','), 'utf8')
-    .digest('hex');
-  return {
-    action: action,
-    client_id: process.env.WITHINGS_API_CLIENT_ID,
-    nonce: nonce,
-    signature: signature,
-  };
-}
-
-async function requestNonse(): Promise<string> {
-  // see this: https://developer.withings.com/api-reference/#tag/signature
-  const signaturetimestamp = Math.floor(new Date().getTime() / 1000);
-  const signatureObj = {
-    action: 'getnonce',
-    client_id: process.env.WITHINGS_API_CLIENT_ID,
-    timestamp: signaturetimestamp,
-    signature: createHmac('sha256', process.env.WITHINGS_API_SECRET)
-      .update(['getnonce', process.env.WITHINGS_API_CLIENT_ID, signaturetimestamp].join(','), 'utf8')
-      .digest('hex'),
-  };
-  const nonceRes = await axios.post('https://wbsapi.withings.net/v2/signature', stringify(signatureObj));
-  return nonceRes.data.body.nonce.toString();
-}
-
-async function requestRefreshAccessToken(refresh_token: string): Promise<AxiosResponse<any, any>> {
-  const basicSignature: RequestTokenSignatureBasic = await constructNonceSignature('requesttoken');
-  const requestTokenObj = {
-    grant_type: 'refresh_token',
-    refresh_token: refresh_token,
-    client_secret: process.env.WITHINGS_API_SECRET,
-    ...basicSignature,
-  };
-  return axios.post('https://wbsapi.withings.net/v2/oauth2', stringify(requestTokenObj));
 }

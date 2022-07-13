c3317d8dfb12c9e3c009ee18197669086f9a28e5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-07-14 01:45:14 +0900

    test

--- server/src/routes/withings/webhook.ts
+++ server/src/routes/withings/webhook.ts
@@ -28,13 +28,22 @@ export async function withingsWebhookRouter(app, opts): Promise<void> {
     const mesureBodyData = await withingsApi.requestAndSaveLatestMesureData();
     return mesureBodyData;
   });
-  app.post('/recieves', async (req, res) => {
+  app.post('/recieves', async (request, res) => {
     console.log('recieved!!!!');
-    console.log(req.headers);
-    console.log(req.raw);
-    console.log(req.body);
-    console.log(req.query);
-    console.log(req.params);
+    console.log(request.body)
+    console.log(request.query)
+    console.log(request.params)
+    console.log(request.headers)
+    console.log(request.raw)
+    console.log(request.server)
+    console.log(request.id)
+    console.log(request.ip)
+    console.log(request.ips)
+    console.log(request.hostname)
+    console.log(request.protocol)
+    console.log(request.url)
+    console.log(request.routerMethod)
+    console.log(request.routerPath)
     /*
     これをparseしてこうする
     {
@@ -44,8 +53,8 @@ export async function withingsWebhookRouter(app, opts): Promise<void> {
       appli: '1'
     }
     */
-    if (req.body) {
-      const payload = parse(req.body);
+    if (request.body) {
+      const payload = parse(request.body);
       const withingsApi = await constructWithingsApi(payload.userid.toString());
       const mesureBodyData = await withingsApi.requestAndSaveLatestMesureData();
       const withingsAccount = withingsApi.getWithingsAccount();
